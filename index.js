c5e23f1ef72ea423dd1027c18babcd3baffafe79
riki-nakayashiki <riki.nakayashiki@gmail.com>
2023-08-16 18:25:55 -0700

    Add blackjack

--- index.js
+++ index.js
@@ -3,26 +3,26 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const textBodyParser = bodyParser.text({ limit: '20mb', defaultCharset: 'utf-8'});
+const textBodyParser = bodyParser.text({ limit: '20mb', defaultCharset: 'utf-8' });
 const port = 3000;
 
-const {authenticateAnswer} = require('./my-modules/question.js');
-
+const { authenticateAnswer } = require('./my-modules/question.js');
+const { readCsvFile, shuffle } = require('./my-modules/blackjack-server.js');
 
 app.use(cors({
-    origin: 'http://localhost:5000'
+    origin: 'http://localhost:5001'
 }));
 
 app.options('/', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5001');
     res.header('Access-Control-Allow-Headers', 'task');
     res.header('Access-Control-Allow-Methods', 'GET');
     res.sendStatus(200);
 });
 
-app.get('/', async function(req, res) {
+app.get('/', async function (req, res) {
 
-    console.log('req.headers: ', req.headers); 
+    console.log('req.headers: ', req.headers);
 
     const reqOrigin = req.headers['origin'];
     const reqTask = req.headers['task'];
@@ -37,14 +37,14 @@ app.get('/', async function(req, res) {
 
         if (authResult == true) {
             res.setHeader('Access-Control-Allow-Origin', '*');
-            res.setHeader('Access-Control-Expose-Headers', 'request-result'); 
+            res.setHeader('Access-Control-Expose-Headers', 'request-result');
             res.setHeader('request-result', 'Request ' + req.method + ' was received successfully.');
 
 
             res.send("right answer!!");
-        } else if (authResult == false){
+        } else if (authResult == false) {
             res.send('wrong answer :/');
-        } 
+        }
     }
     catch (error) {
         console.log('authenticateUser() error:', error);
@@ -52,6 +52,36 @@ app.get('/', async function(req, res) {
     }
 
     res.end();
+});
+
+app.get('/blackjack', async function (req, res) {
+    // print the HTTP Request Headers
+    console.log('req.headers: ', req.headers);
+
+    const reqOrigin = req.headers['origin']; // get the origin of the request
+    const reqTask = req.headers['task']; // get the task of the request
+
+    console.log("Processing request from " + reqOrigin + " for route " + req.url + " with method " + req.method + " for task: " + reqTask);
+
+    // TASK Check
+    if (reqTask === 'blackjack') {
+        try {
+            // readCsvFile returns an array of arrays with the data from the .csv
+            let csvFileData = await readCsvFile('./data/Cards.csv');
+            csvFileData = shuffle(csvFileData);
+
+            // prepare and send the response to the client:
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            // allow client to access the custom 'request-result' header:
+            res.setHeader('Access-Control-Expose-Headers', 'request-result');
+            // set the custom header 'request-result'
+            res.setHeader('request-result', 'Request ' + req.method + ' was received successfully.');
+            res.status(200).json(csvFileData);
+        } catch (error) {
+            console.log('There was a problem: ', error);
+            res.status(500).send("Server Error");
+        }
+    }
 });
 
 app.listen(port, () => {

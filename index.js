5a08de2f418b4a6b034ce46c3e1d8c896ef2f967
riki-nakayashiki <riki.nakayashiki@gmail.com>
2023-08-17 15:45:23 -0700

    Added blackjack

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-const { readCsvFile, shuffle } = require('./my-modules/blackjack-server.js');
+const {
+    readCsvFile,
+    shuffle,
+    getUserInfo
+} = require('./my-modules/blackjack-server.js');
 
 app.use(cors({
     origin: 'http://localhost:5000'
@@ -20,7 +24,7 @@ app.options('/', (req, res) => {
 
 app.get('/blackjack', async function (req, res) {
     // print the HTTP Request Headers
-    console.log('req.headers: ', req.headers);
+    // console.log('req.headers: ', req.headers);
 
     const reqOrigin = req.headers['origin']; // get the origin of the request
     const reqTask = req.headers['task']; // get the task of the request
@@ -42,6 +46,44 @@ app.get('/blackjack', async function (req, res) {
             res.status(200).json(csvFileData);
         } catch (error) {
             console.log('There was a problem: ', error);
+            res.status(500).send("Server Error");
+        }
+        return;
+    }
+    if (reqTask === 'coin') {
+        try {
+            let userInfo = await getUserInfo();
+
+            // prepare and send the response to the client:
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            // allow client to access the custom 'request-result' header:
+            res.setHeader('Access-Control-Expose-Headers', 'request-result');
+            // set the custom header 'request-result'
+            res.setHeader('request-result', 'Request ' + req.method + ' was received successfully.');
+            res.status(200).json(userInfo);
+        } catch (error) {
+            console.log('There was a problem: ', error);
+            res.status(500).send("Server Error");
+        }
+        return;
+    }
+});
+
+app.post('/blackjack', async function (req, res) {
+    const reqOrigin = req.headers['origin']; // get the origin of the request
+    const reqTask = req.headers['task']; // get the task of the request
+
+    console.log("Processing request from " + reqOrigin + " for route " + req.url + " with method " + req.method + " for task: " + reqTask);
+
+    if (reqTask === 'coin') {
+        try {
+            console.log(req);
+            // const username = reqBody.username;
+            // const password = reqBody.password;
+            // await addUser(filePath, username, password);
+
+        } catch (error) {
+            console.log('There was a problem responding with a rotation: ', error);
             res.status(500).send("Server Error");
         }
     }
